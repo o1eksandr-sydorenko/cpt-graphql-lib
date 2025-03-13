06005b6ac06fc997120ccd7ca5a3d7c180fd73d1
Oleksandr Sydorenko <o.sydorenko.work@gmail.com>
2025-03-13 12:09:06 +0200

    Add pagination limit

--- src/inputs/paginate.input.ts
+++ src/inputs/paginate.input.ts
@@ -1,10 +1,12 @@
-import { IsInt, IsOptional } from 'class-validator';
+import { IsInt, IsOptional, Max } from 'class-validator';
 import { Field, InputType } from 'type-graphql';
+import { maxPaginateLimit } from '../config';
 
 @InputType()
 export class PaginateInput {
   @Field(() => Number, { nullable: true })
   @IsInt()
+  @Max(maxPaginateLimit)
   @IsOptional()
   limit?: number;
 
